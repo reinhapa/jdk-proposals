209fbbcd642834d216fc3928968d3269cd431ef9
Patrick Reinhart <patrick@reini.net>
2014-11-12 13:08:35 +0100

    reformated java style

--- IOUtil/src/test/java/net/reini/io/IOUtilTest.java
+++ IOUtil/src/test/java/net/reini/io/IOUtilTest.java
@@ -14,49 +14,49 @@ import org.junit.Test;
 
 public class IOUtilTest {
 
-	@Test
-	public void copyIoStreamsUsingIntConsumer() throws IOException {
-		AtomicLong counter = new AtomicLong();
-		ByteArrayInputStream source = new ByteArrayInputStream("abc".getBytes());
-		ByteArrayOutputStream target = new ByteArrayOutputStream();
+    @Test
+    public void copyIoStreamsUsingIntConsumer() throws IOException {
+        AtomicLong counter = new AtomicLong();
+        ByteArrayInputStream source = new ByteArrayInputStream("abc".getBytes());
+        ByteArrayOutputStream target = new ByteArrayOutputStream();
 
-		copy(source, target, c -> counter.addAndGet(c));
+        copy(source, target, c -> counter.addAndGet(c));
 
-		assertEquals(3, counter.longValue());
-		assertEquals("abc", target.toString());
-	}
+        assertEquals(3, counter.longValue());
+        assertEquals("abc", target.toString());
+    }
 
-	@Test
-	public void copyIoStreamsGetttingReadBytes() throws IOException {
-		ByteArrayInputStream source = new ByteArrayInputStream("abc".getBytes());
-		ByteArrayOutputStream target = new ByteArrayOutputStream();
+    @Test
+    public void copyIoStreamsGetttingReadBytes() throws IOException {
+        ByteArrayInputStream source = new ByteArrayInputStream("abc".getBytes());
+        ByteArrayOutputStream target = new ByteArrayOutputStream();
 
-		long readBytes = copy(source, target);
+        long readBytes = copy(source, target);
 
-		assertEquals(3, readBytes);
-		assertEquals("abc", target.toString());
-	}
+        assertEquals(3, readBytes);
+        assertEquals("abc", target.toString());
+    }
 
-	@Test
-	public void copyIoReadersUsingIntConsumer() throws IOException {
-		AtomicLong counter = new AtomicLong();
-		StringReader source = new StringReader("abc");
-		StringWriter target = new StringWriter();
+    @Test
+    public void copyIoReadersUsingIntConsumer() throws IOException {
+        AtomicLong counter = new AtomicLong();
+        StringReader source = new StringReader("abc");
+        StringWriter target = new StringWriter();
 
-		copy(source, target, c -> counter.addAndGet(c));
+        copy(source, target, c -> counter.addAndGet(c));
 
-		assertEquals(3, counter.longValue());
-		assertEquals("abc", target.toString());
-	}
+        assertEquals(3, counter.longValue());
+        assertEquals("abc", target.toString());
+    }
 
-	@Test
-	public void copyIoReadersGetttingReadBytes() throws IOException {
-		StringReader source = new StringReader("abc");
-		StringWriter target = new StringWriter();
+    @Test
+    public void copyIoReadersGetttingReadBytes() throws IOException {
+        StringReader source = new StringReader("abc");
+        StringWriter target = new StringWriter();
 
-		long readChars = copy(source, target);
+        long readChars = copy(source, target);
 
-		assertEquals(3, readChars);
-		assertEquals("abc", target.toString());
-	}
+        assertEquals(3, readChars);
+        assertEquals("abc", target.toString());
+    }
 }
